09d2bf44f4a0575755af69a8505ee945a60a68b6
Harry37x2 <bartek3737@o2.pl>
2023-02-15 15:54:57 +0100

    simple example

--- src/App.js
+++ src/App.js
@@ -4,25 +4,21 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
-// redux stuff
 
+// redux stuff
 import { createStore } from "redux";
+import {DECREASE, INCREASE} from './actions'
+import reducer from "./reducer";
+
 // initial store
 const initialStore = {
-  count: 37
-}
-// reducer
-function reducer(state, action){
-  if(action.type === 'DECREASE'){
-    console.log('decrease')
-    return {count: state.count - 1}
-  }
-  return state;
-}
+  count: 37,
+  name: 'Harry'
+};
+
 const store = createStore(reducer, initialStore);
-store.dispatch({type: 'DECREASE'})
-store.dispatch({type: 'INCREASE'})
-store.dispatch({type: 'RESET'})
+store.dispatch({type: DECREASE})
+store.dispatch({type: INCREASE})
 console.log(store.getState())
 
 function App() {
---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+import {DECREASE, INCREASE} from './actions'
+
+function reducer(state, action){
+  if(action.type === DECREASE){
+    return {
+      ...state,
+      count: state.count - 1
+    }
+  }
+  if(action.type === INCREASE){
+    return {
+      ...state,
+      count: state.count + 1
+    }
+  }
+  return state;
+}
+
+export default reducer;
